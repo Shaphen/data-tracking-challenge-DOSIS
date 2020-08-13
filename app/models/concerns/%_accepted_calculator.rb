9faa9e8f454cdacf63821c73a4c4c2ebcf0180ab
Shaphen <swpburn@gmail.com>
2020-08-12 20:26:58 -0700

    Add skeleton logic for calculating accepted percentage logic into model concerns

---
+++ app/models/concerns/%_accepted_calculator.rb
@@ -0,0 +1,15 @@
+module PercentageAccepted
+  extend ActiveSupport::Concern
+
+  def self.calculate(nectar, advisement)
+    # insert logic here
+  end
+  
+end
+
+# To use this in any model file:
+# At the top of desired file - includes PercentageAccepted
+# Use - PercentageAccepted.calculate(input1, input2)
+
+# To use the same approach for controller navigate to controllers/concerns
+# and follow the same steps
