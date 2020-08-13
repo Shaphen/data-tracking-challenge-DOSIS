5977f59798df3216438427aa9a889b3dc7a56f9b
Shaphen <swpburn@gmail.com>
2020-08-13 15:06:33 -0700

    Add bio for worker bees

--- app/controllers/combs_controller.rb
+++ app/controllers/combs_controller.rb
@@ -5,5 +5,10 @@ class CombsController < ApplicationController
   end
 
   def show
+    # @comb_memberships = CombStaffMember.where(worker_id: params[:id]) # find all membershipts
+    # @active_membership = @comb_memberships.order(:created_at).last # find active membership (last assigned comb)
+    # @worker_bee = WorkerBee.find_by(id: params[:id]) # get worker bee for info display
+    # @comb = Comb.find_by(id: @active_membership.comb_id)
+    render :show
   end
 end
--- app/controllers/worker_bees_controller.rb
+++ app/controllers/worker_bees_controller.rb
@@ -4,5 +4,14 @@ class WorkerBeesController < ApplicationController
     @worker_bees = WorkerBee.all
     render :index
   end
+
+  def show
+    @comb_memberships = CombStaffMember.where(worker_id: params[:id]) # find all membershipts
+    @active_membership = @comb_memberships.order(:created_at).last # find active membership (last assigned comb)
+    @worker_bee = WorkerBee.find_by(id: params[:id]) # get worker bee for info display
+    @comb = Comb.find_by(id: @active_membership.comb_id)
+    @supervisor = WorkerBee.find_by(id: @comb.supervisor_id)
+    render :show
+  end
   
 end
