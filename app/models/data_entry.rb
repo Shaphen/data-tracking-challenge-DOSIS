57da167c52aef134ed184407f03962510edfa7c4
Shaphen <swpburn@gmail.com>
2020-08-17 16:44:34 -0700

    Fix seeding bug for data entries

--- app/models/data_entry.rb
+++ app/models/data_entry.rb
@@ -1,6 +1,8 @@
 class DataEntry < ApplicationRecord
   validates :worker_id, :date, :pollen_globs, :nectar, presence: true
-  validates :advisement, numericality: { only_integer: true, message: "has to be a number" }
+  validates :advisement, numericality: { 
+    only_integer: true, message: "has to be a number", allow_nil: true
+  }
 
   belongs_to :bee,
     foreign_key: :worker_id,
